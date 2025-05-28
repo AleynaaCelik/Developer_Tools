79e0e058b03ab87f8fb4ed5fbad4d9d3a56f0abd
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-05-28 23:47:34 +0300

    Enums Created

---
+++ DevTools.Core/Enums/IssueCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Core.Enums
+{
+    public enum IssueCategory
+    {
+        Performance = 1,
+        Security = 2,
+        Style = 3,
+        Logic = 4,
+        Memory = 5,
+        Threading = 6,
+        Exception = 7,
+        Validation = 8,
+        Architecture = 9,
+        Testing = 10
+    }
+}
---
+++ DevTools.Core/Enums/SuggestionCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Core.Enums
+{
+    public enum SuggestionCategory
+    {
+        Performance = 1,
+        Readability = 2,
+        Maintainability = 3,
+        BestPractices = 4,
+        Refactoring = 5,
+        Testing = 6,
+        Documentation = 7,
+        Architecture = 8
+    }
+}
