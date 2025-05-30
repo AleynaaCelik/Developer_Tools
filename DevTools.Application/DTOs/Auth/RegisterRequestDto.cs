91d8a2b1fa3f73bd7221aae65a9667fd4d165beb
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-05-30 22:45:34 +0300

    RegisterRequestDto Created

---
+++ DevTools.Application/DTOs/Auth/RegisterRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Application.DTOs.Auth
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
