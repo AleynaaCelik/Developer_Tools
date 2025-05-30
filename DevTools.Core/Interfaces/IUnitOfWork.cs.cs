9b7487a24e78a51e0d6500a41e689a7bb591c76c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-05-30 22:28:56 +0300

    Interfaces Added

---
+++ DevTools.Core/Interfaces/IUnitOfWork.cs.cs
@@ -0,0 +1,21 @@
+﻿using DevTools.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICodeAnalysisSessionRepository CodeAnalysisSessions { get; }
+        IUserProjectRepository UserProjects { get; }
+        IRepository<T> Repository<T>() where T : class;
+
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
---
+++ DevTools.Core/Interfaces/Repositories/IUserProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using DevTools.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Core.Interfaces.Repositories
+{
+    public interface IUserProjectRepository : IRepository<UserProject>
+    {
+        Task<IEnumerable<UserProject>> GetByUserIdAsync(Guid userId);
+        Task<UserProject?> GetByUserIdAndNameAsync(Guid userId, string name);
+        Task<IEnumerable<UserProject>> GetActiveProjectsByUserIdAsync(Guid userId);
+    }
+}
