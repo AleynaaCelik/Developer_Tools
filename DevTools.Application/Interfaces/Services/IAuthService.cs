9b1283716ee7f816580e57055f8f41a29b80b720
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-06-01 23:51:54 +0300

    IAuthService Created

---
+++ DevTools.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using DevTools.Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request);
+        Task<AuthResponseDto> LoginAsync(LoginRequestDto request);
+        Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+        Task<bool> RevokeTokenAsync(string token);
+        Task<UserInfoDto?> GetUserInfoAsync(Guid userId);
+    }
+}
