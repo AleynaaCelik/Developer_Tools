04b79c3ced4a3a6723f3a2a0bd46920c452d69de
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-06-02 22:39:20 +0300

    ITokenService Created

---
+++ DevTools.Application/Interfaces/External/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using DevTools.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Application.Interfaces.External
+{
+    public interface ITokenService
+    {
+        string GenerateJwtToken(User user);
+        string GenerateRefreshToken();
+        Task<bool> ValidateTokenAsync(string token);
+        Task<Guid?> GetUserIdFromTokenAsync(string token);
+    }
+}
