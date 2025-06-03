29048b6f4580e86009562e6eea7af0d90ee7d1f1
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-06-03 23:47:06 +0300

    ApplicationDbContext Created

---
+++ DevTools.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,71 @@
+﻿using DevTools.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CodeAnalysisSession> CodeAnalysisSessions { get; set; }
+        public DbSet<UserProject> UserProjects { get; set; }
+        public DbSet<CodeIssue> CodeIssues { get; set; }
+        public DbSet<CodeSuggestion> CodeSuggestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Apply configurations
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new CodeAnalysisSessionConfiguration());
+            builder.ApplyConfiguration(new UserProjectConfiguration());
+            builder.ApplyConfiguration(new CodeIssueConfiguration());
+            builder.ApplyConfiguration(new CodeSuggestionConfiguration());
+
+            // Identity table customizations
+            builder.Entity<IdentityRole<Guid>>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
+
+            // Seed data
+            SeedData(builder);
+        }
+
+        private void SeedData(ModelBuilder builder)
+        {
+            var adminRoleId = Guid.NewGuid();
+            var userRoleId = Guid.NewGuid();
+
+            builder.Entity<IdentityRole<Guid>>().HasData(
+                new IdentityRole<Guid>
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                },
+                new IdentityRole<Guid>
+                {
+                    Id = userRoleId,
+                    Name = "User",
+                    NormalizedName = "USER",
+                    ConcurrencyStamp = Guid.NewGuid().ToString()
+                }
+            );
+        }
+    }
+}
