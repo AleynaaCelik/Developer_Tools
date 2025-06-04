cb02c015c21c998f7bb3cd96ab86e654a9ea8883
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-06-04 23:23:42 +0300

    UserConfiguration Created

---
+++ DevTools.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using DevTools.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTools.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(e => e.FirstName).HasMaxLength(100);
+            builder.Property(e => e.LastName).HasMaxLength(100);
+            builder.Property(e => e.ApiUsageLimit).HasDefaultValue(100);
+            builder.Property(e => e.ApiUsageUsed).HasDefaultValue(0);
+            builder.Property(e => e.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(e => e.UpdatedAt).HasDefaultValueSql("GETUTCDATE()");
+
+            // Relationships
+            builder.HasMany(u => u.CodeAnalysisSessions)
+                   .WithOne(s => s.User)
+                   .HasForeignKey(s => s.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.UserProjects)
+                   .WithOne(p => p.User)
+                   .HasForeignKey(p => p.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
